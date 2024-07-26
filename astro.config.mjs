5873fda24ed2d3833176df6a8bbbddf0647892d5
Natsumi.H <natsmy.1211@gmail.com>
2024-07-26 20:43:23 +0800

    tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,14 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  // ...
+  integrations: [
+    tailwind({
+      // Example: Provide a custom path to a Tailwind config file
+      //   configFile: "./custom-config.mjs",
+      nesting: true,
+    }),
+  ],
+});
